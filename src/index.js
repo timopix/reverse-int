36bb20fd8c174231958c6e02e1954594b71816ab
timopix <57803428+timopix@users.noreply.github.com>
2023-02-24 22:23:55 -0700

    feat: add reverse-int function, now works faster

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+class Reverser {
+  static reverse(n) {
+    if (typeof n === 'string' || !Number.isInteger(n)) {
+      throw new Error('Invalid argument');
+    }
+    const reversedStr = [...Math.abs(n).toString()].reverse().join('');
+    const reversedNum = Number(reversedStr);
+    return n >= 0 ? reversedNum : reversedNum;
+  }
 }
+
+module.exports = Reverser.reverse;
